f28b49238de75e2d8d8e5178ad315e7b0da13cc6
Maxime Dahirel <maxime.dahirel@yahoo.fr>
2023-01-17 19:04:56 +0100

    building up to interactive validation of data

--- heartbeat.jl
+++ heartbeat.jl
@@ -17,11 +17,32 @@ f = import_video(filename)
 ROI = select_ROI(f)
 df = get_values(ROI, f)
 
-#example plot
-Plots.plot(df[:,"time"], df[:,"valueR"])
-xlabel!("time in sec")
-ylabel!("average value on red channel")
-title!("heartbeat")
+#example plot by channel
+pR = Plots.plot(df[:,"time"], df[:,"valueR"],lc=:red); #semi colon suppress return; plot not displayed for now
+xlabel!("time in sec");
+ylabel!("average value");
+title!("red");
+
+pG = Plots.plot(df[:,"time"], df[:,"valueG"],lc=:green);
+xlabel!("time in sec");
+ylabel!("average value");
+title!("green");
+
+pB = Plots.plot(df[:,"time"], df[:,"valueB"],lc=:blue);
+xlabel!("time in sec");
+ylabel!("average value");
+title!("blue");
+
+pRGB = plot(pR,pG,pB, layout=(2,2),size=(1000,800));
+
+
+io=PipeBuffer()
+png(pRGB,io::IO)
+ii=load(io);
+imshow(ii; canvassize = (1000, 800), name = "Select ROI")
+
+
+
 
 exportpath = save_file(split(filename,".")[1]; filterlist = "csv")
 CSV.write(exportpath, df)
