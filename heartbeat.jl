60f62747f6daafd3c592e2f1e9d7f490ecd084cc
Maxime Dahirel <maxime.dahirel@yahoo.fr>
2023-01-17 10:35:56 +0100

    added example plot

--- heartbeat.jl
+++ heartbeat.jl
@@ -7,6 +7,7 @@ using ProgressBars
 using Statistics
 using VideoIO
 using NativeFileDialog
+using Plots
 
 include("functions/functions.jl")
 
@@ -15,6 +16,12 @@ filename = pick_file(filterlist="mp4")
 f = import_video(filename)
 ROI = select_ROI(f)
 df = get_values(ROI, f)
+
+#example plot
+Plots.plot(df[:,"time"], df[:,"valueR"])
+xlabel!("time in sec")
+ylabel!("average value on red channel")
+title!("heartbeat")
 
 exportpath = save_file(split(filename,".")[1]; filterlist = "csv")
 CSV.write(exportpath, df)
